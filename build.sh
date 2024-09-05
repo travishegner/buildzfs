f22029e47e98e280f749623d95f3a91ad5f2bb29
Travis Hegner <travis.hegner@gmail.com>
2024-09-05 10:42:57 -0400

    updates

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 NUM_CPUS="${NUM_CPUS:=4}"
 ZFS_GPG_KEY="${ZFS_GPG_KEY:=6AD860EED4598027}"
-ZFS_RELEASE="${ZFS_RELEASE:=2.2.5}"
+ZFS_RELEASE="${ZFS_RELEASE:=2.2.6}"
 OLD_ZFS_SHA="9790905f7683d41759418e1ef3432828c31116654ff040e91356ff1c21c31ec0"
-NEW_ZFS_SHA="2388cf6f29cd75e87d6d05e4858a09d419c4f883a658d51ef57796121cd08897"
+NEW_ZFS_SHA="c92e02103ac5dd77bf01d7209eabdca55c7b3356aa747bb2357ec4222652a2a7"
 
 if ls *.zst 1> /dev/null 2>&1; then
   echo "Found existing packages in current directory, please remove before executing."
